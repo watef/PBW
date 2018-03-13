c29d50dfb0513e97a6ff27e2e4f2afc587605999
watef <lulukwatef76@gmail.com>
2018-03-13 23:00:22 +0700

    edit2

--- send_mail.php
+++ send_mail.php
@@ -1,33 +1,4 @@
-<?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "datapbw";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-
-$name=$_POST['name'];
-$email=$_POST['email'];
-//$subject=$_POST['subject'];
-$message=$_POST['message'];
-
-
-$sql = "INSERT INTO datapbw (nama, email, message)
-VALUES ('".$name."', '".$email."','".$message."')";
-
-if ($conn->query($sql) === TRUE) {
-    echo "";
-    
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-}
-
-$conn->close();
 ?>
 <?php
 $name=$_POST['name'];
